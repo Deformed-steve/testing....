b27be77f00b85c2619817e32dae9551e108cdc60
deformed-steve <koltnapier2014@gmail.com>
2025-09-16 19:08:30 -0400

    Update stars.js

--- public/stars.js
+++ public/stars.js
@@ -1,36 +1,31 @@
-const canvas = document.getElementById("stars");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
 let stars = [];
 
-function resizeCanvas() {
+function resize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-}
-window.addEventListener("resize", resizeCanvas);
-resizeCanvas();
-
-for (let i = 0; i < 150; i++) {
-  stars.push({
+  stars = Array.from({ length: 100 }, () => ({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
-    r: Math.random() * 2,
-    d: Math.random() * 0.5
-  });
+    size: Math.random() * 2,
+    speed: Math.random() * 0.5 + 0.2
+  }));
 }
+window.addEventListener('resize', resize);
+resize();
 
-function drawStars() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
+function animate() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'white';
   stars.forEach(s => {
+    s.y += s.speed;
+    if (s.y > canvas.height) s.y = 0;
     ctx.beginPath();
-    ctx.arc(s.x, s.y, s.r, 0, Math.PI*2);
+    ctx.arc(s.x, s.y, s.size, 0, Math.PI * 2);
     ctx.fill();
-    s.y += s.d;
-    if (s.y > canvas.height) {
-      s.y = 0;
-      s.x = Math.random() * canvas.width;
-    }
   });
+  requestAnimationFrame(animate);
 }
-
-setInterval(drawStars, 30);
+animate();
